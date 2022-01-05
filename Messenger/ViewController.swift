9e4234eb1125dad6b25e96f5142d1460972f983d
jengyang07 <jengyang@fple.com>
2022-01-06 01:51:20 +0800

    Getting Started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Jayden Kong on 06/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
